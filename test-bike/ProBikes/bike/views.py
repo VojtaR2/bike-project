6ab92c55fc1b90150a8941d6258a4c8ca043d392
VojtechRastak <149658727+VojtaR2@users.noreply.github.com>
2025-06-03 12:24:12 +0200

    Add files via upload

---
+++ test-bike/ProBikes/bike/views.py
@@ -0,0 +1,31 @@
+
+# Create your views here.
+from django.shortcuts import render
+from .forms import KonfiguratorForm
+from .models import GalleryImage
+
+def index(request):
+    vysledna_cena = None
+    if request.method == 'POST':
+        form = KonfiguratorForm(request.POST)
+        if form.is_valid():
+            ram = form.cleaned_data['ram']
+            vidlice = form.cleaned_data['vidlice']
+            brzdy = form.cleaned_data['brzdy']
+            prehazovacka = form.cleaned_data['prehazovacka']
+            vysledna_cena = sum([
+                ram.cena,
+                vidlice.cena,
+                brzdy.cena,
+                prehazovacka.cena
+            ])
+    else:
+        form = KonfiguratorForm()
+
+    return render(request, 'bike/index.html', {
+        'form': form,
+        'vysledna_cena': vysledna_cena
+    })
+def gallery_view(request):
+    images = GalleryImage.objects.all().order_by('-uploaded_at')
+    return render(request, 'bike/gallery.html', {'images': images})
